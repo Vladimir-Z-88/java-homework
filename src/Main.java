6018f39888ddaa44e4e94f34f48fb63f6d79bfe9
Vladimir-Z-88 <100123616+Vladimir-Z-88@users.noreply.github.com>
2022-04-02 23:48:01 +0300

    Add hw_1 and hw_2

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import hw_1.HomeWork_1;
+import hw_2.HomeWork_2;
+
+public class Main {
+    public static void main(String[] args) {
+//        HomeWork_1 homeWork_1 = new HomeWork_1();
+//
+//        homeWork_1.printEvenOrOddNumber(0);
+//        homeWork_1.printEvenOrOddNumber(1);
+//        homeWork_1.printEvenOrOddNumber(2);
+//        homeWork_1.printEvenOrOddNumber(3);
+//        homeWork_1.printEvenOrOddNumber(40);
+//
+//        homeWork_1.printNumberCloserToTen(7.9f, 14.4f);
+//        homeWork_1.printNumberCloserToTen(-7.9f, 14.4f);
+//        homeWork_1.printNumberCloserToTen(7.9f, -14.4f);
+//        homeWork_1.printNumberCloserToTen(1f, 1f);
+//        homeWork_1.printNumberCloserToTen(0f, 14.4f);
+//        homeWork_1.printNumberCloserToTen(0f, 0f);
+//
+//        homeWork_1.printGenericNumberOnBiggerValue();
+//        homeWork_1.printGenericNumberOnBiggerValue();
+//        homeWork_1.printGenericNumberOnBiggerValue();
+//        homeWork_1.printGenericNumberOnBiggerValue();
+
+
+        HomeWork_2 homeWork_2 = new HomeWork_2();
+        homeWork_2.createMatrix(5,8);
+    }
+}
